36f6c618d00513a443bcdfa0b4c42187eb7d3176
AluminiumTech <aluminiumdev@gmail.com>
2017-07-18 18:55:35 +0800

    increased default accuracy level

--- CSMark/Program.cs
+++ CSMark/Program.cs
@@ -8,8 +8,8 @@ namespace CSMark {
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Title = "CSMark 0.11.0.0";
             Console.WriteLine("Welcome to CSMark 0.11.0.0");
-            double maxIterations = 100.0 * 1000 * 1000;
-            string benchAccuracy = "1"; //1 = 100 Million Calcs, 2 = 500 Million Calcs, 3 = 1 Billion Calcs, 4 = 2 Billion Calcs, 5 = 4 Billion Calcs, 6 = 8 Billion Calcs
+            double maxIterations = 500.0 * 1000 * 1000;
+            string benchAccuracy = "1"; //1 = 1 Billion Calcs, 2 = 2 Billion Calcs, 3 = 4 Billion Calcs, 4 = 9 Billion Calcs
             string newCommand;
 
             while (true) {
@@ -59,41 +59,25 @@ namespace CSMark {
                 else if (newCommand == "bench-accuracy") {
                     Console.WriteLine("Welcome to the accuracy configurator.");
                     Console.WriteLine("Choosing a higher accuracy will result in substantially longer benchmarking times.");
-                    Console.WriteLine("Accuracy Level 1 may take 20-40 seconds ldepending on hardware.");
-                    Console.WriteLine("Accuracy Level 2 may take 5-10x longer than Accuracy Level 1.");
-                    Console.WriteLine("Accuracy Level 3 may take 15-20x longer than Accuracy Level 1");
-                    Console.WriteLine("Accuracy Level 4 may take 25-40x longer than Accuracy Level 1.");
-                    Console.WriteLine("Accuracy Level 5 may take 50-90x longer than Accuracy Level 1.");
-                    Console.WriteLine("Accuracy Level 6 may take 100-150x longer than Accuracy Level 1.");
-                    Console.WriteLine("Accuracy Level 7 may take 170-250x longer than Accuracy Level 1.");
-                    Console.WriteLine("Accuracy Levels 2-4 should be used if accuracy is of great concern. Accuracy Levels 5-7 should be used if accuracy is of the utmost importance.");
+                    Console.WriteLine("Accuracy level options: 1-4");
                     Console.WriteLine("Please ENTER the accuracy level you would like to use for the benchmark test.");
                     benchAccuracy = Console.ReadLine();
                     Console.WriteLine("You have selected Accuracy Level " + benchAccuracy);
 
                     else if (benchAccuracy == "1") {
-                        maxIterations = 100.0 * 1000 * 1000;
-                    }
-                    else if (benchAccuracy == "2") {
-                        maxIterations = 500.0 * 1000 * 1000;
-                    }
-                    else if (benchAccuracy == "3") {
                         maxIterations = 1000.0 * 1000 * 1000;
                     }
-                    else if (benchAccuracy == "4") {
+                    else if (benchAccuracy == "2") {
                         maxIterations = 2000.0 * 1000 * 1000;
                     }
-                    else if (benchAccuracy == "5") {
+                    else if (benchAccuracy == "3") {
                         maxIterations = 4000.0 * 1000 * 1000;
                     }
-                    else if (benchAccuracy == "6") {
-                        maxIterations = 6000.0 * 1000 * 1000;
-                    }
-                    else if (benchAccuracy == "7") {
+                    else if (benchAccuracy == "4") {
                         maxIterations = 9000.0 * 1000 * 1000;
                     }
                     else {
-                        maxIterations = 100.0 * 1000 * 1000;
+                        maxIterations = 500.0 * 1000 * 1000;
                     }
 
                     Console.ForegroundColor = ConsoleColor.Green;
