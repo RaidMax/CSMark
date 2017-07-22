530b15c7cb2a3986f6f6add2b267514ecc7a369a
AluminiumTech <aluminiumdev@gmail.com>
2017-07-22 12:29:51 +0800

    cleaned up the code and optimized it

--- CSMark/Program.cs
+++ CSMark/Program.cs
@@ -9,7 +9,7 @@ namespace CSMark {
             Console.Title = "CSMark 0.11.0.0";
             Console.WriteLine("Welcome to CSMark 0.11.0.0");
             double maxIterations = 1000.0 * 1000 * 1000;
-            string benchAccuracy = "1"; //1 = 1 Billion Calcs, 2 = 2 Billion Calcs, 3 = 4 Billion Calcs, 4 = 9 Billion Calcs
+            string benchAccuracy = "1"; //Benchmark Accuracy 1 = 1 Billion Calcs, 2 = 2 Billion Calcs, 3 = 4 Billion Calcs, 4 = 9 Billion Calcs
             string newCommand;
 
             while (true) {
@@ -23,44 +23,19 @@ namespace CSMark {
                 newCommand = Console.ReadLine().ToLower();
 
                 if (newCommand == "bench"){
+                    Console.WriteLine("Would you like to configure the accuracy level of this benchmark?");
+                    Console.WriteLine("ENTER Y or N");
+                    string configure = Console.ReadLine();
                     Console.ForegroundColor = ConsoleColor.Green;
-                    //Single threaded CPU benchmarks         
-                    Console.WriteLine("Starting benchmark. The benchmark tests may take a while.");
-                    bench.startBenchmark_Single(maxIterations);
-                    bench.startBenchmark_Multi(maxIterations);
-                    Console.WriteLine("                                                                             ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("Pythagoras Test Single Threaded Score: " + bench.returnSingleThreadedPythagoras() + " Calculations Per Millisecond");
-                    Console.WriteLine("Trigonometry Test Single Threaded Score: " + bench.returnSingleThreadedTrigonometry() + " Calculations Per Millisecond");
-                    Console.WriteLine("PercentageError Test Single Threaded Score: " + bench.returnSingleThreadedPercentageError() + " Calculations Per Millisecond");
-                    Console.WriteLine("ArithmeticSumN Test Single Threaded Score: " + bench.returnSingleThreadedArithmeticSumN() + " Calculations Per Millisecond");
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    //Multi threaded CPU benchmarks
-                    Console.WriteLine("Pythagoras Test Multi Threaded Score: " + bench.returnMultiThreadedPythagoras() + " Calculations Per Millisecond");
-                    Console.WriteLine("Trigonometry Test Multi Threaded Score: " + bench.returnMultiThreadedTrigonometry() + " Calculations Per Millisecond");
-                    Console.WriteLine("PercentageError Test Multi Threaded Score: " + bench.returnMultiThreadedPercentageError() + " Calculations Per Millisecond");
-                    Console.WriteLine("ArithmeticSumN Test Multi Threaded Score: " + bench.returnMultiThreadedArithmeticSumN() + " Calculations Per Millisecond");
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    //Benchmark scaling
-                    Console.WriteLine("Improvements compared to Single Threaded Performance: ");
-                    Console.WriteLine("Pythagoras Test Improvement: " + bench.returnScalingPythagoras().ToString() + "%");
-                    Console.WriteLine("Trigonometry Test Improvement: " + bench.returnScalingTrigonometry().ToString() + "%");
-                    Console.WriteLine("PercentageError Test Improvement: " + bench.returnScalingPercentageError().ToString() + "%");
-                    Console.WriteLine("ArithmeticSumN Test Improvement: " + bench.returnScalingArithmeticSumN().ToString() + "%");
-                    Console.WriteLine("CPU Thread count: " + Environment.ProcessorCount.ToString());
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    Console.WriteLine("Benchmark Accuracy: " + benchAccuracy);
-                    continue;
-                }
-                else if (newCommand == "bench-accuracy") {
-                    Console.WriteLine("Welcome to the accuracy configurator.");
-                    Console.WriteLine("Choosing a higher accuracy will result in substantially longer benchmarking times.");
-                    Console.WriteLine("Accuracy level options: 1-4");
-                    Console.WriteLine("Please ENTER the accuracy level you would like to use for the benchmark test.");
+                    if (configure.ToLower() == "y")
+                    {
+                        Console.WriteLine("Welcome to the accuracy configurator.");
+                        Console.WriteLine("Choosing a higher accuracy will result in substantially longer benchmarking times.");
+                        Console.WriteLine("Accuracy level options: 1-4");
+                        Console.WriteLine("Please ENTER the accuracy level you would like to use for the benchmark test.");
                     benchAccuracy = Console.ReadLine();
-                    Console.WriteLine("You have selected Accuracy Level " + benchAccuracy);
-
-                    else if (benchAccuracy == "1"){
+                    
+                    if (benchAccuracy == "1"){
                         maxIterations = 1000.0 * 1000 * 1000;
                     }
                     else if (benchAccuracy == "2"){
@@ -75,38 +50,71 @@ namespace CSMark {
                     else{
                         maxIterations = 1000.0 * 1000 * 1000;
                     }
+                    Console.WriteLine("You have selected Accuracy Level " + benchAccuracy);
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    //Single threaded CPU benchmarks         
-                    Console.WriteLine("Starting benchmark. The benchmark tests may take a while.");
-                    bench.startBenchmark_Single(maxIterations);
-                    bench.startBenchmark_Multi(maxIterations);
-                    Console.WriteLine("                                                                             ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("Pythagoras Test Single Threaded Score: " + bench.returnSingleThreadedPythagoras() + " Calculations Per Millisecond");
-                    Console.WriteLine("Trigonometry Test Single Threaded Score: " + bench.returnSingleThreadedTrigonometry() + " Calculations Per Millisecond");
-                    Console.WriteLine("PercentageError Test Single Threaded Score: " + bench.returnSingleThreadedPercentageError() + " Calculations Per Millisecond");
-                    Console.WriteLine("ArithmeticSumN Test Single Threaded Score: " + bench.returnSingleThreadedArithmeticSumN() + " Calculations Per Millisecond");
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    //Multi threaded CPU benchmarks
-                    Console.WriteLine("Pythagoras Test Multi Threaded Score: " + bench.returnMultiThreadedPythagoras() + " Calculations Per Millisecond");
-                    Console.WriteLine("Trigonometry Test Multi Threaded Score: " + bench.returnMultiThreadedTrigonometry() + " Calculations Per Millisecond");
-                    Console.WriteLine("PercentageError Test Multi Threaded Score: " + bench.returnMultiThreadedPercentageError() + " Calculations Per Millisecond");
-                    Console.WriteLine("ArithmeticSumN Test Multi Threaded Score: " + bench.returnMultiThreadedArithmeticSumN() + " Calculations Per Millisecond");
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    //Benchmark scaling
-                    Console.WriteLine("Improvements compared to Single Threaded Performance: ");
-                    Console.WriteLine("Pythagoras Test Improvement: " + bench.returnScalingPythagoras().ToString() + "%");
-                    Console.WriteLine("Trigonometry Test Improvement: " + bench.returnScalingTrigonometry().ToString() + "%");
-                    Console.WriteLine("PercentageError Test Improvement: " + bench.returnScalingPercentageError().ToString() + "%");
-                    Console.WriteLine("ArithmeticSumN Test Improvement: " + bench.returnScalingArithmeticSumN().ToString() + "%");
-                    Console.WriteLine("CPU Thread count: " + Environment.ProcessorCount.ToString());
-                    Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-                    Console.WriteLine("Benchmark Accuracy: " + benchAccuracy);
+                        Console.WriteLine("Starting benchmark. The benchmark tests may take a while.");
+                        bench.startBenchmark_Single(maxIterations);
+                        bench.startBenchmark_Multi(maxIterations);
+                        Console.WriteLine("                                                                             ");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Pythagoras Test Single Threaded Score: " + bench.returnSingleThreadedPythagoras() + " Calculations Per Millisecond");
+                        Console.WriteLine("Trigonometry Test Single Threaded Score: " + bench.returnSingleThreadedTrigonometry() + " Calculations Per Millisecond");
+                        Console.WriteLine("PercentageError Test Single Threaded Score: " + bench.returnSingleThreadedPercentageError() + " Calculations Per Millisecond");
+                        Console.WriteLine("ArithmeticSumN Test Single Threaded Score: " + bench.returnSingleThreadedArithmeticSumN() + " Calculations Per Millisecond");
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        //Multi threaded CPU benchmarks
+                        Console.WriteLine("Pythagoras Test Multi Threaded Score: " + bench.returnMultiThreadedPythagoras() + " Calculations Per Millisecond");
+                        Console.WriteLine("Trigonometry Test Multi Threaded Score: " + bench.returnMultiThreadedTrigonometry() + " Calculations Per Millisecond");
+                        Console.WriteLine("PercentageError Test Multi Threaded Score: " + bench.returnMultiThreadedPercentageError() + " Calculations Per Millisecond");
+                        Console.WriteLine("ArithmeticSumN Test Multi Threaded Score: " + bench.returnMultiThreadedArithmeticSumN() + " Calculations Per Millisecond");
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        //Benchmark scaling
+                        Console.WriteLine("Improvements compared to Single Threaded Performance: ");
+                        Console.WriteLine("Pythagoras Test Improvement: " + bench.returnScalingPythagoras().ToString() + "%");
+                        Console.WriteLine("Trigonometry Test Improvement: " + bench.returnScalingTrigonometry().ToString() + "%");
+                        Console.WriteLine("PercentageError Test Improvement: " + bench.returnScalingPercentageError().ToString() + "%");
+                        Console.WriteLine("ArithmeticSumN Test Improvement: " + bench.returnScalingArithmeticSumN().ToString() + "%");
+                        Console.WriteLine("CPU Thread count: " + Environment.ProcessorCount.ToString());
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        Console.WriteLine("Benchmark Accuracy: " + benchAccuracy); ;
+                        continue;
+
+                    }
+                    else if (configure.ToLower() == "n"){
+                        //Start the benchmarks right away if the user doesn't want to configure the accuracy level
+                        Console.WriteLine("Starting benchmark. The benchmark tests may take a while.");
+                        bench.startBenchmark_Single(maxIterations);
+                        bench.startBenchmark_Multi(maxIterations);
+                        Console.WriteLine("                                                                             ");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Pythagoras Test Single Threaded Score: " + bench.returnSingleThreadedPythagoras() + " Calculations Per Millisecond");
+                        Console.WriteLine("Trigonometry Test Single Threaded Score: " + bench.returnSingleThreadedTrigonometry() + " Calculations Per Millisecond");
+                        Console.WriteLine("PercentageError Test Single Threaded Score: " + bench.returnSingleThreadedPercentageError() + " Calculations Per Millisecond");
+                        Console.WriteLine("ArithmeticSumN Test Single Threaded Score: " + bench.returnSingleThreadedArithmeticSumN() + " Calculations Per Millisecond");
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        //Multi threaded CPU benchmarks
+                        Console.WriteLine("Pythagoras Test Multi Threaded Score: " + bench.returnMultiThreadedPythagoras() + " Calculations Per Millisecond");
+                        Console.WriteLine("Trigonometry Test Multi Threaded Score: " + bench.returnMultiThreadedTrigonometry() + " Calculations Per Millisecond");
+                        Console.WriteLine("PercentageError Test Multi Threaded Score: " + bench.returnMultiThreadedPercentageError() + " Calculations Per Millisecond");
+                        Console.WriteLine("ArithmeticSumN Test Multi Threaded Score: " + bench.returnMultiThreadedArithmeticSumN() + " Calculations Per Millisecond");
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        //Benchmark scaling
+                        Console.WriteLine("Improvements compared to Single Threaded Performance: ");
+                        Console.WriteLine("Pythagoras Test Improvement: " + bench.returnScalingPythagoras().ToString() + "%");
+                        Console.WriteLine("Trigonometry Test Improvement: " + bench.returnScalingTrigonometry().ToString() + "%");
+                        Console.WriteLine("PercentageError Test Improvement: " + bench.returnScalingPercentageError().ToString() + "%");
+                        Console.WriteLine("ArithmeticSumN Test Improvement: " + bench.returnScalingArithmeticSumN().ToString() + "%");
+                        Console.WriteLine("CPU Thread count: " + Environment.ProcessorCount.ToString());
+                        Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                        Console.WriteLine("Benchmark Accuracy: " + benchAccuracy); ;
+                        continue;
+                    }
+                    else {
+                    }        
                     continue;
                 }
                 else if (newCommand == "bench-average"){
-                    Console.ForegroundColor = ConsoleColor.Green;      
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Starting benchmark. The benchmark tests may take a while.");
                     bench.startBenchmark_Average(maxIterations);
                     Console.WriteLine("                                                                             ");
